ae7b180fba20c08f68f18e5d7d6a5bd8ad85d137
Juan Santelices <86135408+jdsantelicesl@users.noreply.github.com>
2023-11-22 09:39:10 -0800

    Edited README

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # SCC Club Website
 
+Welcome to SCC Club Website repository. Please see Documents in order to get started. 
+
 Documents:
 
 [Quickstart](./docs/Quickstart.md)
--- server/app.py
+++ server/app.py
@@ -1,8 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_file
 
-app = Flask(__name__, template_folder="../client/public")
-
+app = Flask(
+    __name__,
+    template_folder="../client/public",
+    static_folder="../client/public",
+    static_url_path="/static",
+)
 
 @app.route("/")
 def index():
-    return render_template("client/index.html")
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(port=8888, debug=True)
