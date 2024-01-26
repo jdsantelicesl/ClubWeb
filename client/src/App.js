a9a1eb40ee3704b776dcb6abbda17db2c7e1643f
Juan Santelices <86135408+jdsantelicesl@users.noreply.github.com>
2024-01-26 13:47:56 -0800

    edited app content

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,9 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        Learn React
-      </header>
+      <h1> Welcome to Programming Club Web</h1>
     </div>
   );
 }
