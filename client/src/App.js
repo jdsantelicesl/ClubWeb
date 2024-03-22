1886254fd1c4b7fa1b1be1f7f5974c447e9d8bd2
Edward <edwardchhun3@gmail.com>
2024-03-22 15:35:33 -0700

    Changed App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App #2">
       <h1> Welcome to Programming Club Web, test2 </h1>
     </div>
   );
