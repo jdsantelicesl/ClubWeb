68048745b6a32d6cdf4d0eba1e79321549a3fdb3
Juan Santelices <86135408+jdsantelicesl@users.noreply.github.com>
2024-03-22 18:45:19 +0000

    test2

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1> Welcome to Programming Club Web, new edit</h1>
+      <h1> Welcome to Programming Club Web, test2 </h1>
     </div>
   );
 }
