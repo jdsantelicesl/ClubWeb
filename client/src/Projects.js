4b8e7ba14dd3fc32873fd2c0c0b0245b7bc41835
Edward <edwardchhun3@gmail.com>
2024-04-05 16:59:11 -0700

    Updated Links

--- client/src/Projects.js
+++ client/src/Projects.js
@@ -6,15 +6,15 @@ const Projects = () => {
       <h1>Projects</h1>
       <div className="project-card">
         <h2>Project 1</h2>
-        <p>This is a description of Project 1.</p>
-        <a href="https://github.com/username/project1" target="_blank" rel="noopener noreferrer">
+        <p>This is the Programming Club Website GitHub</p>
+        <a href="https://github.com/jdsantelicesl/ClubWeb" target="_blank" rel="noopener noreferrer">
           View on GitHub
         </a>
       </div>
       <div className="project-card">
         <h2>Project 2</h2>
-        <p>This is a description of Project 2.</p>
-        <a href="https://github.com/username/project2" target="_blank" rel="noopener noreferrer">
+        <p>This is Juan's RC Car Project using Arduino.</p>
+        <a href="https://github.com/jdsantelicesl/Arduino-RC-Car" target="_blank" rel="noopener noreferrer">
           View on GitHub
         </a>
       </div>
