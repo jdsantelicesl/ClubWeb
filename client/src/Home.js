4343c67df4eb9aeed9a8d78c2e7dcb605cc52c37
lynn <HPi3@nah.nah>
2024-04-22 10:50:19 -0700

    extra deleted svg

--- client/src/Home.js
+++ client/src/Home.js
@@ -2,6 +2,7 @@ export default function Home() {
     return (
         <div className="Home">
             <h1 style={{margin: "2vw"}}> Home Page </h1>
+            <p>Meeting is every friday 3pm-5p Bus 157</p>
         </div>
     );
 }
