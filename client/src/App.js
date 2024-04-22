8a2bfb57803ac8e99b679400c0b8dbeab0f8eeb4
lynn <HPi3@nah.nah>
2024-04-22 13:04:23 -0700

    footer work

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Header from './Header';
 import Footer from './Footer';
+import Footer2 from "./Footer2";
 
 function App() {
   return (
     <>
     <Header />
+    <Footer2 />
     <Footer />
     </>
   );
--- client/src/Footer.js
+++ client/src/Footer.js
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import "./App.css";
 import image2 from "./scc-logo.png"
