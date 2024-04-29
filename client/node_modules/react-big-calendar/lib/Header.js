e868bce723539d53cbaedbc56dee92d405c712c3
lynn <HPi3@nah.nah>
2024-04-28 22:24:37 -0700

    pictures

--- client/node_modules/react-big-calendar/lib/Header.js
+++ client/node_modules/react-big-calendar/lib/Header.js
@@ -5,7 +5,6 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.default = void 0;
-var _propTypes = _interopRequireDefault(require("prop-types"));
 var _react = _interopRequireDefault(require("react"));
 var Header = function Header(_ref) {
   var label = _ref.label;
@@ -14,7 +13,4 @@ var Header = function Header(_ref) {
     "aria-sort": "none"
   }, label);
 };
-Header.propTypes = process.env.NODE_ENV !== "production" ? {
-  label: _propTypes.default.node
-} : {};
 var _default = exports.default = Header;
--- client/src/Members.js
+++ client/src/Members.js
@@ -1,7 +1,30 @@
+import "./App.css";
+
 export default function Members() {
     return (
-        <div className="Members">
-            <h1 style={{margin: "2vw"}}> Members </h1>
+        <div>
+        <h1 style={{margin: "2vw"}}> Members </h1>
+        <div className="memimg">
+            <div>
+            <img className='memimg1' img src="./image/m1.jpg"></img>
+            </div>
+            <div>
+            <img className='memimg1' img src="./image/m2.jpg"></img>
+            </div>
+            <div>
+            <img className='memimg1' img src="./image/m3.jpg"></img>
+            </div>
+            <div>
+            <img className='memimg1' img src="./image/m4.jpg"></img>
+            </div>
+            <div>
+            <img className='memimg1' img src="./image/m5.jpg"></img>
+            </div>
+            <div>
+            <img className='memimg1' img src="./image/m6.jpg"></img>
+            </div>
+
+        </div>
         </div>
     );
 }
